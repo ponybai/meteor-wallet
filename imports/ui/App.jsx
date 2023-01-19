eed12d575f87cf05d8992fc1ac12554c3e49e68c
Pony Bai <ponybai@gmail.com>
2023-01-19 16:25:29 +0800

    Update App.jsx
    add title cloud 2

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,7 +4,7 @@ import {ContactList} from "./ContactList";
 
 export const App = () => (
   <div>
-    <h1>Meteor Wallet - Galaxy</h1>
+    <h1>Meteor Wallet - Galaxy Cloud2</h1>
     <ContactForm />
     <ContactList />
   </div>
